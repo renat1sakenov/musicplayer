8984a20120f5160749ef5401017796dc1d08957c
Renat <renat.sakenov@googlemail.com>
2017-04-14 15:10:55 +0200

    #1

---
+++ src/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+import mmp_gui
+
+def main():
+
+    gui = mmp_gui.Gui()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/mmp_gui.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+import tkinter
+
+class Gui():
+
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.mainloop()

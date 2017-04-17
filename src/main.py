3e254d26f2819a64bf588dcbd9cbecb31e61a574
Renat <renat.sakenov@googlemail.com>
2017-04-17 13:19:06 +0200

    #2

--- src/main.py
+++ src/main.py
@@ -3,12 +3,10 @@
 import mmp_gui
 
 def main():
-
     gui = mmp_gui.Gui()
 
-
-
-
+def msg(text):
+    print(text)
 
 if __name__ == "__main__":
     main()
--- src/mmp_gui.py
+++ src/mmp_gui.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python2
 
-import tkinter
+#import tkinter
+from tkinter import * 
+import main
 
 class Gui():
 
     def __init__(self):
-        self.window = tkinter.Tk()
+        self.window = Tk() 
+        self.window.geometry("%dx%d+0+0" % (self.window.winfo_screenwidth(), self.window.winfo_screenheight()))
+        self.window.wm_title("MMP")
+        self.window["bg"] = "white"
+
+        self.menubar = Menu(self.window)
+        self.menubar.add_command(label="Add Music", command=self.add_music)
+        self.menubar.add_command(label="Options", command=self.options)
+        self.menubar["bg"] = "white"
+        self.menubar["fg"] = "black"
+
+        self.window.config(menu=self.menubar)
         self.window.mainloop()
+
+    def add_music():
+        pass
+
+    def options():
+        pass

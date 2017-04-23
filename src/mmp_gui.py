a2ab722f3fc3623862d21a28064095bd1a5cccbb
Renat <renat.sakenov@googlemail.com>
2017-04-23 14:22:28 +0200

    #8

--- src/mmp_gui.py
+++ src/mmp_gui.py
@@ -15,7 +15,7 @@ class Gui():
         self.HEIGHT = self.window.winfo_screenheight()
         self.WIDTH = self.window.winfo_screenwidth()
         self.COLOR = "#EEFFAA"
-        self.COVER_SIZE = 220
+        self.COVER_SIZE = 150
         self.PADDING_PERCENT = 0.1
 
 
@@ -29,28 +29,28 @@ class Gui():
         self.menubar["bg"] = "white"
         self.menubar["fg"] = "black"
 
-        self.scrollbar = Scrollbar(self.window)
-        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.main_frame = Frame(self.window,height= self.HEIGHT, width= self.WIDTH,relief=SUNKEN)
+        self.main_frame.pack()
 
-        self.main_canvas = Canvas(self.window,height = self.HEIGHT,width = self.WIDTH,bg=self.COLOR,yscrollcommand=self.scrollbar.set)
+        self.main_canvas = Canvas(self.main_frame,height = self.HEIGHT,width = self.WIDTH,bg=self.COLOR,scrollregion=(0,0,0,2000)) 
+
+        self.scrollbar = Scrollbar(self.main_frame,orient=VERTICAL)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.scrollbar.config(command=self.main_canvas.yview)
+
         self.main_canvas.pack()
 
-
-        self.scrollbar.config(command=self.main_canvas.yview)
+        self.main_canvas.config(yscrollcommand=self.scrollbar.set)
         
-        #self.main_frame = Frame(self.main_canvas,height= self.HEIGHT, width= self.WIDTH,bg=self.COLOR,relief=SUNKEN)
-        #self.main_frame.pack()
-
-
         self.window.config(menu=self.menubar)
 
 
     def display(self,artists):
         pad = self.get_padding()
-        x_ = y_ = pad
+        x_ = y_ = pad + 100 
         for artist in artists:
             for album in artist.albums:
-                cover = Canvas(self.main_canvas,width = self.COVER_SIZE, height = self.COVER_SIZE)
+                cover = Canvas(self.main_frame,width = self.COVER_SIZE, height = self.COVER_SIZE)
                 output = io.BytesIO(self.main.get_image(album))
                 output.seek(0)
                 img = Image.open(output)
@@ -59,12 +59,13 @@ class Gui():
                 self.images.append(pimg)
                 cover.create_image(0,0,image=pimg,anchor="nw")
                 cover.pack()
-                cover.place(x = x_, y = y_)
+            
+                self.main_canvas.create_window((x_,y_),width = self.COVER_SIZE, height = self.COVER_SIZE,window = cover)
             
                 if x_ + self.COVER_SIZE + pad  < self.WIDTH - self.COVER_SIZE:
                     x_ += self.COVER_SIZE + pad
                 else:
-                    x_ = pad
+                    x_ = pad + 100
                     y_ += self.COVER_SIZE + pad
         self.window.mainloop()
 

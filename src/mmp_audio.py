7ba56b8ea176cf0be949777ad9ecba4c469cc8c4
Renat Sakenov <renat.sakenov@googlemail.com>
2017-05-26 20:24:47 +0200

    #24

--- src/mmp_audio.py
+++ src/mmp_audio.py
@@ -1,11 +1,6 @@
-import subprocess
-import os
-import time
-from threading import Thread
-
-
 import mmp
 
+from vlc import MediaPlayer as player
 
 class Audio():
 
@@ -17,22 +12,14 @@ class Audio():
 
     def play(self,track):
         if not self.PLAYING_NOW:
-            print("MMP: New track " + str(track))
             self.PLAYING_NOW = True
-            self.playing = subprocess.Popen("mpg123 '" + track+"'\n",shell=True,stdin=subprocess.PIPE)
-            #some kind of polling / interrupt when the track ends, :
-            '''
-            if self.PLAYING_NOW:
-                self.control.next_track()
-            '''
-        else: 
-            print("MMP: Interrupting current track")
+            self.playing = player(track)
+            self.playing.play()
+        else:
+            self.playing.stop()
             self.PLAYING_NOW = False
-            os.system("killall mpg123") #todo..
-            time.sleep(0.1)		#change so that current process really died
             self.play(track)
 
     def pause(self):
-        print("trying now!")
-        #self.playing.communicate(b's')[0]
-        self.playing.stdin.write(b's')[0]
+        self.playing.pause()
+       
